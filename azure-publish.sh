38771f23817d0ca5b0489188aa72b0f0519ab55f
Dominik Szymański <domints@gmail.com>
2023-01-14 15:20:01 +0100

    Revert "Move to using zip file as artifact"
    This reverts commit e0957bcee459cda58296601e2d3c04a9350cc9c9.

--- azure-publish.sh
+++ azure-publish.sh
@@ -5,7 +5,7 @@ sudo /usr/bin/systemctl_stop gtfs && \
 echo "GTFS service stopped. Removing old app" && \
 rm -rf /var/dotnet/gtfs/* && \
 echo "Old version removed. Unzipping artifacts" && \
-unzip artifact.tar.gz -d /var/dotnet/gtfs && \
+tar -xzf artifact.tar.gz -C /var/dotnet/gtfs && \
 echo "Artifacts unzipped. Starting GTFS service..." && \
 sudo /usr/bin/systemctl_start gtfs && \
 echo "Deployment script finished!"
