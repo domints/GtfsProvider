e0957bcee459cda58296601e2d3c04a9350cc9c9
Dominik Szymański <domints@gmail.com>
2023-01-14 15:13:38 +0100

    Move to using zip file as artifact

--- azure-publish.sh
+++ azure-publish.sh
@@ -5,7 +5,7 @@ sudo /usr/bin/systemctl_stop gtfs && \
 echo "GTFS service stopped. Removing old app" && \
 rm -rf /var/dotnet/gtfs/* && \
 echo "Old version removed. Unzipping artifacts" && \
-tar -xzf artifact.tar.gz -C /var/dotnet/gtfs && \
+unzip artifact.tar.gz -d /var/dotnet/gtfs && \
 echo "Artifacts unzipped. Starting GTFS service..." && \
 sudo /usr/bin/systemctl_start gtfs && \
 echo "Deployment script finished!"
